2514e87ec306509c50f67f5c82a6bf62e7e605f7
Elias <Elias@bastubacken.se>
2019-10-09 16:54:46 +0200

    Ny-New-Elias

--- src/New.js
+++ src/New.js
@@ -1,10 +1,14 @@
 import React, { useState } from 'react';
-import {SaveStudy, OpenStudy, NameFree} from './DB.js';
+import DB from './DB.js';
 import Popup from "reactjs-popup";
 import {CreateGrid} from './Study.js';
 import {Redirect} from "react-router-dom";
 
-
+const contentStyle = {
+	background: "#F0F0F0",
+	width: "400px",
+	border: "none"
+};
 
 // <NewButton changeName={setStudy} empty={setData} startEdit={setEdit}/>
 //   f={RemoveStudy} confirm={true} study={props.study} close={close}
@@ -16,31 +20,41 @@ function NewButton(props){
 	return(
 		<>
 		{select && <Redirect to='/Edit' />}
-		<Popup trigger={<button>Create</button>} position={'bottom center'}>
+		<Popup 
+		trigger={<button className="button_pop">Create</button>} 
+		modal
+		contentStyle={contentStyle}
+		>
 		{close =>(
-			<>
+			<div className="modal">
 			{nameAvailable ? 'Choose study name' : 'Name exists'}
             <input
-                type="text"
+				type="text"
+				className="Text-input"
                 value={props.study.name}
                 onChange={event => props.setStudy(props.study.changeName(event.target.value))}
 		    />
+
 			Recipe datasheet dimensions
-			<DimInput dim={[0,0]} dims={dims} setDims={setDims}/>
+			<div className="pop_div">
+			<DimInput  dim={[0,0]} dims={dims} setDims={setDims}/>
 			<DimInput dim={[0,1]} dims={dims} setDims={setDims}/>
+			</div>
 			Consumer datasheet dimensions
+			<div className="pop_div">
 			<DimInput dim={[1,0]} dims={dims} setDims={setDims}/>
 			<DimInput dim={[1,1]} dims={dims} setDims={setDims}/>
-
+			</div>
 			{/* CONFIRMATION BUTTONS */}
+			<div className="pop_div">
 			<ConfirmButton label={'Confirm'} 
 			f = {arg => {
-				if(NameFree(arg))
+				if(DB.NameFree(arg))
 				{	
-					SaveStudy(props.study
+					DB.SaveStudy(props.study
 						.changeRecipe(CreateGrid(dims[0][0],dims[0][1]))
 						.changeConsumer(CreateGrid(dims[1][0],dims[1][1])));
-					props.setStudy(OpenStudy(arg))
+					props.setStudy(DB.OpenStudy(arg))
 					setAvailable(true)
 					setSelect(true)
 					close()
@@ -50,7 +64,8 @@ function NewButton(props){
             }} arg={props.study.name}
             />
 			<ConfirmButton label={'Close'} close={close}/>
-			</>
+			</div>
+			</div>
 		)}
 		</Popup>
 		</>
@@ -65,6 +80,7 @@ function ConfirmButton({label, f=null, arg, close=null}){
 		<input
 		type="button"
 		value={label}
+		className="button_pop"
 		onClick={event => 
 			{	
 				if(f !== null){f(arg)};
@@ -78,6 +94,7 @@ function ConfirmButton({label, f=null, arg, close=null}){
 function DimInput({dim, dims, setDims}){
 	return(
 		<input
+			className="Dimmer"
 			type="number"
 			value={dims[dim[0]][dim[1]]>0?dims[dim[0]][dim[1]]:''}
 			onChange={event => 
