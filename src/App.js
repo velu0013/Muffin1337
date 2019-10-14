24c66b1c85c532f1290543066b50465141b5095f
Felix <f3liix@hotmail.com>
2019-10-14 14:24:51 +0200

    TestAndroid

--- src/App.js
+++ src/App.js
@@ -6,12 +6,12 @@
 
 import React, { useState } from 'react';
 import './App.css';
-import {RoutePage} from './Study_CRUD/Routepage.js'
-import {Homepage} from './Study_CRUD/Home.js'
+import {RoutePage} from './Study_CRUD/Routepage.js';
+import {Homepage} from './Study_CRUD/Home.js';
 
 
 function App() {
-  return(<RoutePage/>);
+  return <RoutePage/>;
 }
 
 
