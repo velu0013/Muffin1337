16159a686496bfcc29e7767fcc10970279236162
Felix <f3liix@hotmail.com>
2019-10-14 14:35:24 +0200

    TestAndroid

---
+++ src/Analysis/Analysis_Master.js
@@ -0,0 +1,12 @@
+const Analyzer = [ 
+  {name: 'analys1', 
+  description: 'testar setup', 
+  component: analyze1}, 
+  {name: 'analys2', 
+  description: 'test igen', 
+  component: analyze1}
+ ]; 
+
+function analyze1(props){
+  return 1;
+}
