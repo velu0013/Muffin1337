1fac19f9b89a30f6115ea5b5eeb9935a001a2867
katrinalindmark <katannlindmark@gmail.com>
2019-11-08 12:16:31 +0100

    clustersidan

--- src/Analysis/Components/Cluster.js
+++ src/Analysis/Components/Cluster.js
@@ -1,20 +1,66 @@
-import React from 'react'
+import React, { useState , useEffect} from 'react'
 import {kmeans} from '../Methods/kmeans.js'
+import Popup from "reactjs-popup";
 
 function ClusterAnalysis({study, close}){
-    let recipe = study.getRecipeTabular()
-    let consumer = study.getConsumerTabular()
-    let preference = study.getPreferenceTabular()
-    let testdata = [1, 2, 3, 1, 2, 3, 7, 8, 9, 7, 8, 9, 7, 8, 9, 1, 2, 3, 1, 2, 3, 7, 8, 9];
+    const [param, setParam] = useState(null)
+    let recipe = study.getRecipeTabular();
+    let consumer = study.getConsumerTabular();
+    let preference = study.getPreferenceTabular();
+    const nrClusters = 2;
+    const data = setData(preference);
+
+
     return (
         <>
+        <ParameterSelector paramList={study.getHeader('preference')} param={param} setParam={setParam}/>
+        <ParameterSelector paramList={study.getHeader('recipe')} param={param} setParam={setParam}/>
         En annan analys på {study.name} som delar in data i kluster. 
-        {kmeans(testdata)}
+        {kmeans(data, nrClusters)}
+        {console.log(recipe)}
         <br></br>
         <input type="button" className="info_pop" value="Back" onClick={close}/>
         </>
     ); 
 }
+
+function setData(input){
+    var i = 0;
+    var data = [];
+    for(i = 0; i <= input.length - 1; i++){
+        data[i] = [input[i][6], input[i][7]];
+    }
+    return data;
+}
+
+function ParameterSelector({paramList, param, setParam}){
+    const label = param===null?'Select Parameter':'Analyzing: '+param;
+	return(
+        <Popup trigger={<button className="button_pop">{label}</button>} 
+            position={'right top'}
+            closeOnDocumentClick
+            mouseLeaveDelay={300}
+            mouseEnterDelay={0}
+            on='hover'
+            contentStyle={{ padding: "0px", border: "none" }}
+            arrow={false}
+        >
+		{close => (
+            <>
+            {paramList.map((value, index) => {
+                return <ul key={index} className="dropdown-item">
+                    {<div onClick={event => {setParam(value); close();}}>
+                        {value}
+                    </div>}
+                </ul>
+            })}
+            </>
+        )}
+        </Popup>
+	)
+}
+
+//Funk för vad som händer på onclick:
 
 export default ClusterAnalysis
 
--- src/Analysis/Methods/kmeans.js
+++ src/Analysis/Methods/kmeans.js
@@ -1,5 +1,7 @@
-// naive k-means kluster analysis, kräver antal , klarar hur många dim som helst
+import { mixedTypeAnnotation } from "@babel/types";
 
+// naive k-means kluster analysis, kräver antal , klarar hur många dim som helst
+//Behöver all data från en observation, inte från en variabel :(
 /*To do:
     hur många kluster? vart definiera? gör så att clusterassignment kan ta godtyckligt antal (nu bara 2)
     hur många max iterationer ska jag ta??
@@ -8,6 +10,225 @@
     välj tol i convergencetest
     gör 2d
 */
+
+
+
+function kmeans(data, nrClusters){
+    var oldClusterCenter = generateRandom(data, nrClusters); //vector with length number of clusters. Each entry contains vec length *dim*
+    var clusterCenters = generateRandom(data, nrClusters);
+    var count = 0;
+
+    while(convergenceTest(clusterCenters, oldClusterCenter, count)==false){
+        count = count + 1;
+        var c = clusterAssignment(data, clusterCenters);
+        oldClusterCenter = clusterCenters
+        clusterCenters = setClusterCenters(c, data);
+    }
+
+ 
+    return nameClustercenter(c); 
+    
+}
+
+function generateRandom(data, nrClusters){
+    var clusterCenters = [];
+    var i = 0;
+    for(i = 0; i <= nrClusters - 1; i++){
+        var clusterCoordinates = [];
+        var j = 0;
+        for(j = 0; j <= data[0].length - 1; j++){
+            clusterCoordinates[j] = Math.random();
+        }
+        clusterCenters[i] = clusterCoordinates;
+    }
+    return clusterCenters;
+}
+
+
+
+function convergenceTest(clusterCenter, oldClusterCenter, iteration){
+    const tol = 0.1;
+    var maxIterations = 100;
+    var isConverged = true;
+    var i = 0;
+    for(i = 0; i<=clusterCenter.length - 1; i++){
+        if(findDistance(oldClusterCenter[i], clusterCenter[i])>tol) isConverged = false;
+    }
+
+    if(iteration==maxIterations) isConverged = true;
+    return isConverged;
+}
+
+
+
+function clusterAssignment(data, clusterCenters){
+        // Returnera en matris c: 
+        //c har en vektor för varje klustercenter
+        //varje klusters vektor innehåller längden från varje datapkt till centret. 0 för de som inte tillhör det klustret 
+        //kom ihåg! Varje datapunkt kan ha flera dims
+        //behandla data som vektor med en vektor för varje datapunkt. 
+        //data[0].length = antal variabler
+        //data.length = antal observationer
+        var c = [];
+        var i = 0;
+        for(i = 0; i<=clusterCenters.length - 1; i++){
+            var distFromPoint = [];
+            var j = 0;
+            for(j = 0; j<=data.length - 1; j++){
+                //distFromPoint[j] = findDistance(clusterCenters[j], data[i]);
+                distFromPoint[j] = findDistance(clusterCenters[i], data[j]);
+            }
+            c[i] = distFromPoint;
+        }
+
+
+        var i = 0;
+        for(i = 0; i <= data.length - 1; i++){
+            var observationToCenter = [];
+            var j = 0;
+            for(j = 0; j <= clusterCenters.length - 1; j++){
+                observationToCenter[j] = c[j][i];
+            }
+
+ 
+            var j = 0;
+            for(j = 0; j <= clusterCenters.length - 1; j++){
+                if(Math.min.apply(null, observationToCenter) != c[j][i]) c[j][i] = c[j][i];
+                else c[j][i] = 0;
+            }
+        
+        }
+
+        /*
+        var i = 0;
+        for(i = 0; i<= clusterCenters.length - 1; i++){
+            var j = 0;
+            for(j = 0; j<= data.length - 1; j++){
+                if(Math.min.apply(null, c[i]) != c[i][j])  c[i][j] = c[i][j];
+                else c[i][j] = 0;
+            }
+        }
+        */
+
+        return c;
+}
+
+
+
+
+function findDistance(A, B){
+    //A is an arrays corresponding to one datapoint. length is length of analysisdimension. Usually 2.
+    //B is array corresponding to one cluster.  -||-
+    var i = 0;
+    var distance = 0;
+    for (i = 0; i <=A.length-1; i++){ //loop over every dimension. Usually 2.
+        var diff = A[i] - B[i];
+        distance = distance + Math.pow(diff,2);
+    }
+    return Math.sqrt(distance);
+}
+
+
+
+//gå igenom varje kluster, c har en vek för varje kluster
+//för visst kluster, gå igenom varje variabel och summera för den variabeln
+//dela varje summa med antal non zero
+// M = en vek/kluster, varje vek har ett värde per variabel
+
+
+
+//c = (antalkluster, antalobservationer)
+//c = [[längd varje obs till det klustret],     [-||-],...]
+//M = (antal kluster, antal variabler)
+//M = [[kluster1 koordinater],      [kluster2koordinater],...]
+function setClusterCenters(c, data){
+    var M = [];
+    var i = 0;
+    for(i = 0; i <= c.length - 1; i++){ //gå igenom varje kluster
+        var clusterCoordinates = [];
+        var j = 0;
+        for(j = 0; j <= data[0].length - 1; j++){ //gå igenom varje variabel
+            clusterCoordinates[j] = 0;
+            var k = 0;
+            for(k = 0; k <= data.length - 1; k++){ //gå igenom varje datapunkt
+                if(c[i][k] != 0)    clusterCoordinates[j] = clusterCoordinates[j] + data[k][j];
+            }
+            if(countNonzero(c[i]) != 0) clusterCoordinates[j] = clusterCoordinates[j]/countNonzero(c[i]);
+            else clusterCoordinates[j] = 0;
+        }
+        M[i] = clusterCoordinates;
+    }
+    return M;
+}
+/*
+function setClusterCenters(c, data){
+    //c = [avst variabel1 för varje observation]    [avst variabel1 för varje observation] ...
+    var M = [];
+    var temp = [];
+    var i = 0;
+    for(i = 0; i <= c.length - 1; i++){ //gå igenom varje kluster
+        var j = 0;
+        for(j = 0; j <= data.length - 1; j++){ // gå igenom varje observation
+            var k = 0;
+            for(k = 0; k <= data[0].length - 1; k++){ //gå igenom varje variabel i den observationen
+                M[i][k] = M[i][k] + data[j][k]; 
+            }
+        }
+        M[i] = M[i]/countNonzero(c[i]);
+
+    }
+    return M;
+}
+*/
+
+
+function countNonzero(vector){
+    var count = 0;
+    var i = 0;
+    for (i=0; i<=vector.length-1; i++){
+        if(Math.abs(vector[i])>0) count = count + 1;
+    }
+    return count;
+}
+
+
+
+function nameClustercenter(c){
+    var clusterName = [];
+    // för varje kluster: gå igenom motsvarande c. om c inte = 0 sätt clustername[m(datapnkt)] = det klustret
+    //c har en vek per kluster. Varje vek innehåller dist från viss observation
+    var i = 0;
+    for(i = 0; i <= c[0].length - 1; i++){
+        var j = 0;
+        for (j = 0; j <= c.length - 1; j++){
+            if(c[j][i] != 0) clusterName[i] = j;
+        }
+    }
+
+    return clusterName;
+}
+
+// Gå igenom varje observation i
+// för varje observation, gå igenom varje kluster j
+//om c[j][i] inte = 0, clusterName[i] = j
+
+
+export {kmeans} 
+
+
+
+
+/*
+// naive k-means kluster analysis, kräver antal , klarar hur många dim som helst
+
+//To do:
+//    hur många kluster? vart definiera? gör så att clusterassignment kan ta godtyckligt antal (nu bara 2)
+//    hur många max iterationer ska jag ta??
+//    slumpa första klustercenter + i setclustercenter slumpa om ett kluster inte innehåller några datapunkter
+//    motverka att klustercenter är samma?? (knas i clusterassignmentfunktion annars)
+//    välj tol i convergencetest
+//    gör 2d
+
 
 
 
@@ -54,11 +275,11 @@ function clusterAssignment(data, clusterCenters){
         for (j = 0; j<=data.length-1; j++){ //loopa över varje datapunkt
             var l = 0
             for (l = 0; l<=k-1; l++){
-                vectorOfLengths[l] = Math.abs(data[j] - clusterCenters[l])
+                vectorOfLengths[l] = getDistance([data[j]], [clusterCenters[l]])
             }
             var nearest = Math.min.apply(null, vectorOfLengths)
-            if(nearest == Math.abs(data[j] - clusterCenters[i])){
-                temp[j] = Math.abs(data[j] - clusterCenters[i]); //Temp ska va ci[j]. är den det? ja :(
+            if(nearest == getDistance([data[j]], [clusterCenters[i]])){
+                temp[j] = getDistance([data[j]], [clusterCenters[i]]); //Temp ska va ci[j]. är den det? ja :(
             } else {
                 temp[j] = 0;
             }
@@ -84,17 +305,18 @@ function findDistance(A, B){
     return Math.sqrt(distance);
 }
 
-/*
-    skriv om    Math.abs(data[j] - clusterCenters[l]) till
-    sqrt(   (data[1][1]-clustercenters[1][1])^2     +    (data[1][2]-clustercenters[1][2])^2    +...    )
-*/
-
 function getDistance(dataPoint, clusterCenter){
     //dataPoint är en vektor med ett värde för varje dimension
     //clustercenter är en vektor med ett värde för varje dimension
     var i = 0;
     var distance = 0;
-
+    var difference = 0;
+    for(i = 0; i <= dataPoint.length - 1; i++){
+        difference = dataPoint[i] - clusterCenter[i];
+        distance = distance + Math.pow(difference,2)
+    }
+    distance = Math.pow(distance, 0.5)
+    return distance
 }
 
 
@@ -144,3 +366,4 @@ function nameClustercenter(c){
 
 
 export {kmeans} 
+*/
