9f3f740a951e4fe4802dd21ff6b63cfe2f863c12
John <john96.isaksson@gmail.com>
2019-11-18 13:09:52 +0100

    En icke-fungerande PCA grej

--- src/Analysis/Analysis_Master.js
+++ src/Analysis/Analysis_Master.js
@@ -1,5 +1,6 @@
 import ClusterAnalysis from './Components/Cluster.js'
 import ParameterRelevance from './Components/ParameterRelevance.js'
+import PCA1 from './Components/PCA-component'
 
 const Analyzers = [
   {name: 'Cluster Analysis', 
@@ -10,6 +11,11 @@ const Analyzers = [
   {name: 'Parameter Relevance', 
   component: ParameterRelevance,
   description: 'Uses machine learning to try and predict a parameter based on patterns in the data. The relevance of a parameter is then based on the success rate of the predictions.'
+  }
+  ,
+  {name: 'PCA test',
+  component: PCA1,
+  description: 'Creates principal components'
   }
 ];
 
