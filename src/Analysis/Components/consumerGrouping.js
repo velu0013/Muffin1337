3f5abdabb2dea706f6f543e5486063454c25eca6
katrinalindmark <katannlindmark@gmail.com>
2019-11-20 15:57:43 +0100

    kmode: standardisera, slumpa, sortera

--- src/Analysis/Components/consumerGrouping.js
+++ src/Analysis/Components/consumerGrouping.js
@@ -1,21 +1,203 @@
 import React, { useState , useEffect} from 'react'
-import {kmeans} from '../Methods/kmeans.js'
+import {consumerClusters} from '../Methods/kmode.js'
 import Popup from "reactjs-popup";
 import Chart from "react-apexcharts";
 import StudyTable from '../../Study_CRUD/StudyTable.js';
 
 function consumerGroups({study, close}){
-
+    let consumerData = study.getConsumerTabular();
+    let k = 2;
     
     return (
         <>
-            Goddag
+            Goddag {consumerClusters(consumerData, k)}
         </>
     ); 
 }
 
 
+
+function makeConsumerTable(consumer, clusterID, clusterChoice, study){
+    //clusterID= output från k-means, consumer= consumertabellen, clusterChoice= vilket cluster man vill titta på.
+    let displayTable = [];
+    displayTable[0] = study.getConsumerHeader();
+    let i;
+        for(i = 1; i < clusterID.length + 1; i++){
+            if(clusterID[i] === clusterChoice) displayTable.push(consumer[i]);
+        }
+    console.log('displaytable')
+    console.log(displayTable)
+    return displayTable;
+}
+
+//clusteredConsumers
+function displayConsumerTable(clusteredConsumers){   
+        console.log('clusteredTable')
+        console.log(clusteredConsumers)
+        return(
+        <>
+        <br></br>
+       <header className="Table-fix">
+            <br></br>
+            Consumerdata cluster A
+            <StudyTable 
+                tableKey = {'_cons'}
+                tableData={clusteredConsumers} 
+            />
+            <br></br>
+        </header>
+        </>
+        )   
+}
+/*
+                setData={x =>{
+                    const newStudy = study.changeConsumer(x)
+                    DB.setCurrentStudy(newStudy)
+                    setStudy(newStudy)
+                }}
+*/
+
+
+function makeSeries(clustersLabels, k, data, headers){
+    const options= {
+            chart: {
+                zoom: {
+                    enabled: true,
+                    type: 'xy'
+                }
+            },
+            xaxis: {
+                tickAmount: 10,
+                labels: {
+                    formatter: function(val) {
+                        return parseFloat(val).toFixed(1)
+                    }
+                }
+            },
+            yaxis: {
+                tickAmount: 7
+            }
+        };
+    let series = [];
+    for(let s=0; s<k; s++){
+        series.push({name: headers[s],
+        data: []})
+    }
+    for (let r=0; r<clustersLabels.length; r++){
+        series[clustersLabels[r]].data.push(data[r])
+    }
+    console.log('series:')
+    console.log(series)
+    return(
+        <Chart options={options} series={series} type="scatter" className="Cluster-chart"  width="98%" height="350"/>
+
+    )
+}
+
+
+function setData(input, indices){
+    var i = 0, c = 0;
+    var data = [];
+    let row = [];
+    for(i = 0; i < input.length; i++){
+        row = [];
+        for (c = 0; c < indices.length; c++){
+            row.push(input[i][indices[c]])
+        }
+        data.push(row);
+    }
+    return data;
+}
+
+function KSelector({k, setK}){
+    const ks = [2,3,4,5];
+    const label = 'Using k: '+k;
+	return(
+        <Popup trigger={<button className="button_pop">{label}</button>} 
+            position={'right top'}
+            closeOnDocumentClick
+            mouseLeaveDelay={30}
+            mouseEnterDelay={0}
+            on='hover'
+            contentStyle={{ padding: "0px", border: "none" }}
+            arrow={false}
+        >
+		{close => (
+            <>
+            {ks.map((value, index) => {
+                return <ul key={index} className="dropdown-item">
+                    {<div onClick={event => {setK(value); close();}}>
+                        {value}
+                    </div>}
+                </ul>
+            })}
+            </>
+        )}
+        </Popup>
+	)
+}
+
+
+function ParameterSelector({paramList, param, setParam}){
+    const label = param===null?'Select Parameter':'Analyzing: '+param;
+	return(
+        <Popup trigger={<button className="button_pop">{label}</button>} 
+            position={'right top'}
+            closeOnDocumentClick
+            mouseLeaveDelay={30}
+            mouseEnterDelay={0}
+            on='hover'
+            contentStyle={{ padding: "0px", border: "none" }}
+            arrow={false}
+        >
+		{close => (
+            <>
+            {paramList.map((value, index) => {
+                return <ul key={index} className="dropdown-item">
+                    {<div onClick={event => {setParam(value); close();}}>
+                        {value}
+                    </div>}
+                </ul>
+            })}
+            </>
+        )}
+        </Popup>
+	)
+}
+
+
+
+
+
+
+
+
 export default consumerGroups
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
    // return <input type="button" value="Ctrl+Z" onClick={close}/>
     //let recipe = study.getRecipe()
--- src/Analysis/Methods/kmode.js
+++ src/Analysis/Methods/kmode.js
@@ -1,22 +1,57 @@
 // Mixed algorithm i PDF s.9
-function consumerClusters(dataIn){
-    //let sorted = sortByType();
-    //let data = sorted[0];
-    //let nrNum = sorted[1]
+function consumerClusters(dataIn, k){
+    let sorted = sortByType(dataIn); //returns [[sortedData],   [nrNumeric]]
+    let data = sorted[0];
+    let nrNum = sorted[1]
+    //data = standardizeData(data, nrNum);
+    //standardize data here
+    let clusterCenters = randomizeCenter(data, nrNum, k);
+    let oldClusterCenters = randomizeCenter(data, nrNum, k);
+    console.log('randomized centers')
+    console.log(oldClusterCenters)
 
+    return 5  
 }
 
 
-/*
+function randomizeCenter(data, nrNum, k){
+    //takes a dataset, it can contain both numeric and catagorical data as long as all numeric are in the beginning
+    //nrNum = number of numerical variables
+    //k = desired number of clusters
+    //returns k randomized centers with nrNum dimensions. Each dimension is uniformly distributed over min-max of data
+    let clusterCenters = [];
+    let thisVariable = [];
+    let i, j, max, min;
+    for(i = 0; i < nrNum; i++){
+        thisVariable = []
+        for(j = 0; j < k; j++){
+            min = Math.min.apply(null, data[i]);
+            max = Math.max.apply(null, data[i]);
+            thisVariable.push(Math.random()*(max - min) + min);
+        }
+        clusterCenters.push(thisVariable);
+    }
+    return clusterCenters
+}
+
+
+
+
+//////////////Convert data of form [[consumer 1], [consumer2],...] to the form [[age], [gender]] and so on
+/////////////Also all numeric vectors will be in the beginning of matrix
+/////////////Also standardizes it (i.e mean 0 and stdev 1)
+
 function sortByType(data){
-    let i, sortedData, categData;
+    let i;
+    let categData = [];
     let nrNum = 0;
-    for(i = 0; i < data.length; i++){
-        if(type(data[i][0]) === 'numeric'){
-            sortedData.push(data[i]);
+    let sortedData = [];
+    for(i = 0; i < data[0].length; i++){
+        if(data[0][i] - data[0][i] === 0){
+            sortedData.push(getColumn(data, i));
             nrNum = nrNum + 1;
         }else{
-            categData.push(data[i]);
+            categData.push(getColumn(data, i));
         }
     }
 
@@ -26,13 +61,34 @@ function sortByType(data){
 
     return [sortedData, nrNum];
 }
-*/
 
 
 
 
+function getColumn(matrix, column){
+    let i;
+    let columnVec = [];
+    for(i = 0; i < matrix.length; i++){
+        columnVec[i] = matrix[i][column];
+    }
+    return columnVec
+}
 
 
+function standardizeData(data, nrNum){
+    //såhär skriver man ej sum! läs hur här https://codeburst.io/javascript-arrays-finding-the-minimum-maximum-sum-average-values-f02f1b0ce332
+    let i, mean, stdev;
+    let standardData = [];
+    for(i = 0; i < nrNum; i++){
+        mean = Math.sum(data[i])/data[i].length;
+        stdev = Math.sqrt(Math.sum(data[i].map(x => Math.pow((x - 1), 2))))/data[i].length;
+        standardData.push(data[i].map(x => (x - mean)/stdev));
+    }
+    return standardData
+    //stdev =sqrt( sum( (x - mean)^2 ) / count(x))
+    //let newVec = data[0].map(x => Math.pow((x - 1), 2));
+    //data = (data - mean)/stdev;
+}
 
 
 
