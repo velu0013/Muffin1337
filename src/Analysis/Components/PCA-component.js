caed4121857ea9cb9ed1fc3d66a9788b905d8715
John <john96.isaksson@gmail.com>
2019-11-28 15:52:02 +0100

    Lagt till en loading-grej

--- src/Analysis/Components/PCA-component.js
+++ src/Analysis/Components/PCA-component.js
@@ -1,25 +1,35 @@
 import React from 'react';
-import {PCA1,PCA2} from '../Methods/PCA-test'
+import {PCA1,PCA2,Loading} from '../Methods/PCA-test'
 import Chart from "react-apexcharts";
 
 //Tar in preference-tabellen och räknar ut egenvärdet/egenvektorerna
 
 function PCAcomp(study) {
     const data = study.study.getTabular('preference')
+    const data5 = study.study.getPreferenceHeader()
     const data2 = PCA2(data)
+    const test = Loading(data5)
     
-    
-    
+    const test2 = LoadingCalc(data2,test)
     const res1= 
         data2.map(({vector})=>(vector))
     
     const xaxis = res1[0]
     const yaxis = res1[1]
     const score = Dot(data,xaxis,yaxis)
-
-    return PCAchart(score)
+    return(
+    <>
+    Scoreplot
+    {PCAchart(score)}
+    <br></br>
+    Loadingplot
+    {Loadingchart(test2)}
+    </>
+    )
 }
 
+//En funktion som tar datapunkterna samt de två första egenvektorerna.
+//Alla datapunkter blir projicerade på de två egenvektorerna och utifrån dessa beräknas scores
 function Dot(data, xaxis, yaxis) {
     const scores= []
     var i 
@@ -83,11 +93,77 @@ function PCAchart(score){
     
     
     
-    console.log('series')
-    console.log(seriescol)
+    //console.log('series')
+    //console.log(seriescol)
     return(
         <Chart options={options} series={series} type="scatter" className="Cluster-chart"  width="98%" height="350"/>
 
+    )
+}
+function Dotproduct(tal1,tal2){
+    var i; 
+    const prod = []
+    var prod1 = 0
+    for(i=0; i<tal1.length; i++){
+        prod1= prod1 + (tal1[i]*tal2[i])
+        
+    }
+    prod.push(prod1)
+    return prod
+}
+
+
+function LoadingCalc(eig, load) {
+    const loadinglist1 = []
+    const loadinglist2 = []
+    var i;
+    const eigenval= 
+        eig.map(({eigenvalue})=>(eigenvalue))
+    const eigenvec= 
+        eig.map(({vector})=>(vector))
+    const axel=
+        load.map(({axis})=>(axis))
+    const name=
+        load.map(({name})=>(name))
+    for(i=0; i<2; i++){
+        var j;
+        var loading = []
+        for(j=0; j<axel.length; j++){
+            loading.push((Dotproduct(eigenvec[i],axel[j])/eigenval[j]))
+        
+        }
+        loadinglist1.push(loading)
+    }
+    const testlist = []
+    for(i=0; i<loadinglist1[0].length; i++) {
+         testlist.push([loadinglist1[0][i],loadinglist1[1][i]])
+        //loadinglist2.push({name:name[i], data: cor})
+
+    }
+    loadinglist2.push({name: 'John',data:testlist })
+    return loadinglist2
+}
+
+function Loadingchart(loading) {
+    const options= {
+        chart: {
+            zoom: {
+                enabled: true,
+                type: 'xy'
+            }
+        },
+        xaxis: {
+            tickAmount: 5,
+            
+            
+        },
+        yaxis: {
+            tickAmount: 5
+        }
+     
+    };
+    return(
+    <Chart options={options} series={loading} type="scatter" className="Cluster-chart"  width="98%" height="350"/>
     )
 }
 
--- src/Analysis/Methods/PCA-test.js
+++ src/Analysis/Methods/PCA-test.js
@@ -2,6 +2,20 @@ import PCA from 'pca-js'
 
 //PCA1 använder en egen matris för test
 
+/*
+Problemen just nu:
+Mina "scores" är inte beräknade med eigenvektorerna som axlar
+utan det är det "gamla" origo som ger koordinaterna. Ett sätt att 
+lösa detta hade kunnat vara att beräkna sträckan mellan punkten på eigenvektorn
+och origo. Vet dock inte hur man ska skilja på positiva och negativa datapunkter 
+relativt till eigenvektorerna
+
+apexcharts gör det svårt att ha en serie med endast datapunkt i. Den tolkar detta som
+två y-koordinater istället för att se det som ett x och ett y. Jag kan skapa scatter-plots
+, men där har datapunkterna ingen form av identifikation
+
+Är osäker ifall mitt sätt att beräkna loadings är korrekt.
+*/
 function PCA1() {
     const vec = [[{value: 2}, {value: 5}, {value: 4},{value: 4}],
                 [{value:3}, {value: 7}, {value: 7}, {value: 7}],
@@ -46,8 +60,28 @@ function PCA2(vec) {
     }
     
     const MeanMatrix = PCA.computeDeviationMatrix(data2)
+    const devscore = PCA.computeDeviationScores(MeanMatrix)
+    console.log(MeanMatrix)
     const vectors = PCA.getEigenVectors(MeanMatrix);
     const topTwo = PCA.computePercentageExplained(vectors,vectors[0])
     return vectors
 }
-export {PCA1, PCA2}
+
+//Loading-funktionen tar in namnet för varje kolumn samt skapar en
+//vektor som ska representera axeln kolumnen utgör
+function Loading(data) {
+    var i
+    const header = []
+    for(i=1;i<data.length; i++) {
+        var arr = Array.apply(null, new Array(data.length-1)).map(Number.prototype.valueOf,0);
+        arr[i-1] = 1
+
+        header.push({name:data[i], axis:arr})
+        
+    }
+    console.log(header)
+    return header
+}
+
+
+export {PCA1, PCA2, Loading }
