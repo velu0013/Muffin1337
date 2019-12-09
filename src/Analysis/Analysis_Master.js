81fbf2dbd94c4fd92cf7f28df307d362037d0574
Felix <f3liix@hotmail.com>
2019-12-09 17:09:42 +0100

    consumer cluster interface started

--- src/Analysis/Analysis_Master.js
+++ src/Analysis/Analysis_Master.js
@@ -2,32 +2,32 @@ import ClusterAnalysis from './Components/Cluster.js'
 import ParameterRelevance from './Components/ParameterRelevance.js'
 import LinearDep from './Components/LinearDep.js'
 import PCAcomp from './Components/PCA-component'
-import consumerGroups from './Components/consumerGrouping.js'
+import ConsumerGroups from './Components/consumerGrouping.js'
 
-const Analyzers = [
-  {name: 'Cluster Analysis', 
-  component: ClusterAnalysis,
-  description: 'Plots clusters'
-  }
-  ,
-  {name: 'Parameter Relevance', 
-  component: ParameterRelevance,
-  description: 'Uses machine learning to try and predict a parameter based on patterns in the data. The relevance of a parameter is then based on the success rate of the predictions.'
-  }
-  ,
-  {name: 'Linear Relation', 
-  component: LinearDep,
-  description: 'Assumes a linear relationship between recipe parameter and preference response and plots each participant as a point according to their preference. Colors points depending on which parameter is selected.'
-  }
- ,
-  {name: 'PCA', 
-  component: PCAcomp,
-  description: 'The method reduces the dimensionality of a dataset using principal component analysis.'
-  }
-  ,
-  {name: 'Consumer clustering', 
-  component: consumerGroups,
-  description: 'Groups consumers by k-mode clustering'
+const Analyzers = [{
+    name: 'Cluster Analysis',
+    component: ClusterAnalysis,
+    description: 'Plots clusters'
+  },
+  {
+    name: 'Parameter Relevance',
+    component: ParameterRelevance,
+    description: 'Uses machine learning to try and predict a parameter based on patterns in the data. The relevance of a parameter is then based on the success rate of the predictions.'
+  },
+  {
+    name: 'Linear Relation',
+    component: LinearDep,
+    description: 'Assumes a linear relationship between recipe parameter and preference response and plots each participant as a point according to their preference. Colors points depending on which parameter is selected.'
+  },
+  {
+    name: 'PCA',
+    component: PCAcomp,
+    description: 'The method reduces the dimensionality of a dataset using principal component analysis.'
+  },
+  {
+    name: 'Consumer clustering',
+    component: ConsumerGroups,
+    description: 'Groups consumers by k-mode clustering'
   }
 ];
 
